a0507f34e3df90c9a51859face97849616178657
unknown <riccardo.tassinari96@gmail.com>
2019-05-20 06:50:26 +0200

    tests

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ cfg = core.Config(core.CONFIG_PATH)
 
 # di questa canzone non legge artist perchè è nullo, molto strano
 # cioè se è nullo non posso assegnarlo, strano
-core.setEyeD3Tags(
+""" core.setEyeD3Tags(
     "D:\\MUSICA\\AM Conspiracy\\(2010) AM Conspiracy\\05 Away.mp3",
     "Away",
     "AM Conspiracy",
@@ -13,4 +13,7 @@ core.setEyeD3Tags(
     5
 )
 
-print("ok")
+print("ok") """
+
+
+print("(ciao)".capitalize())
